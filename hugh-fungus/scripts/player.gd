af708d12beca942e0e74bfacf88bdbf6276a6a09
Breath-of-Fresh-Air <bofa.opensource@gmail.com>
2025-11-04 21:35:50 -0800

    Set up EM wave scene

--- hugh-fungus/scripts/player.gd
+++ hugh-fungus/scripts/player.gd
@@ -5,7 +5,10 @@ extends CharacterBody2D
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	if input_direction.x != 0:
+		velocity.x = input_direction.x * speed
+	else:
+		velocity.x = 0
 
 func _physics_process(_delta):
 	get_input()
---
+++ hugh-fungus/wave_display.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+# Amplitude and wavelength controls
+@export var amplitude: float = 40.0  #height of the wave
+@export var frequency: float = 2.0   #number of cycles across screen width
+@export var phase: float = 0.0       #phase shift left/right
