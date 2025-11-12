c767c0f469bedb5feda35f3bf0920e9cdab11918
Breath-of-Fresh-Air <bofa.opensource@gmail.com>
2025-11-12 00:36:57 -0800

    more work on EM system.

--- hugh-fungus/scripts/wave_display_player.gd
+++ hugh-fungus/scripts/wave_display_player.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-#Amplitude and wavelength controls
-@export var amplitude: float = 40.0  #height of the wave
-@export var frequency: float = 5.0   #number of cycles across screen width
+#target_amplitude and wavelength controls
+@export var target_amplitude: float = 40.0  #height of the wave
+@export var target_frequency: float = 5.0   #number of cycles across screen width
 @export var phase: float = 0.0       #phase shift left/right
-@export var speed: float = 3.5 #directly influences phase shift with a relationship to delta (1/60)
+@export var target_speed: float = 3.5 #directly influences phase shift with a relationship to delta (1/60)
 
 func _process(delta):
 #animate the wave by increasing phase
-	phase += speed * delta
+	phase += target_speed * delta
 	queue_redraw()  #request to redraw the wave every frame
 
 func _draw():
@@ -19,17 +19,17 @@ func _draw():
 
 	
 	for x in range(width):
-		var y = amplitude * sin((x / width) * frequency * TAU + phase)
+		var y = target_amplitude * sin((x / width) * target_frequency * TAU + phase)
 		points_along_sinwave.append(Vector2(x, y))
 	
 	#center the wave vertically
 	for i in range(points_along_sinwave.size() - 1):
 		draw_line(points_along_sinwave[i] + Vector2(0, get_viewport_rect().size.y / 2),
 				  points_along_sinwave[i + 1] + Vector2(0, get_viewport_rect().size.y / 2),
-				  Color(0.967, 0.764, 0.0, 1.0), 6)
+				  Color(0.998, 0.696, 0.756, 1.0), 6, true)
 
-func set_target_wave(A:= amplitude,F:= frequency,P:= phase,S:= speed):
-	amplitude = A
-	frequency = F
+func set_target_wave(A:= target_amplitude,F:= target_frequency,P:= phase,S:= target_speed):
+	target_amplitude = A
+	target_frequency = F
 	phase = P
-	speed = S
+	target_speed = S
