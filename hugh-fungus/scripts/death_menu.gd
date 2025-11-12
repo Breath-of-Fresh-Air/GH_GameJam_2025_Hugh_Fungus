ded8bfb1bb1b9322b6d2faa1384372ff3128a58e
morgan <goombaplayz@gmail.com>
2025-11-11 19:19:37 -0800

    added death stuff

--- hugh-fungus/scripts/death_menu.gd
+++ hugh-fungus/scripts/death_menu.gd
@@ -8,12 +8,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	#change this from player health to a signal from the player its anims are done 
+	if PlayerHealthGlobal.player_health <= 0:
+		self.visible = true
+	else:
+		self.visible = false
 
 func _on_reset_button_button_up() -> void:
 	get_tree().reload_current_scene()
-
+	PlayerHealthGlobal.player_health = 5
 
 func _on_quit_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
