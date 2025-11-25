86300eb07d33d1e3be1ef5df73f52a135f7133e3
morgan <goombaplayz@gmail.com>
2025-11-25 10:00:01 -0800

    Update death_menu.gd

--- hugh-fungus/scripts/death_menu.gd
+++ hugh-fungus/scripts/death_menu.gd
@@ -3,8 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	get_tree().paused = false
+	self.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,15 +16,12 @@ func _process(delta: float) -> void:
 		
 
 func _on_reset_button_button_up() -> void:
-	get_tree().paused = false
-	self.visible = false
+	
 	PlayerHealthGlobal.player_health = 5
 	get_tree().reload_current_scene()
 	
-	self.visible = false
 func _on_quit_button_button_up() -> void:
-	get_tree().paused = false
-	self.visible = false
 	PlayerHealthGlobal.player_health = 5
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	
