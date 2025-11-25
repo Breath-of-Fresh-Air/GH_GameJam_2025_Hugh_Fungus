9b3d1b54aba0461a4946ee873d69bc243d463c86
morgan <goombaplayz@gmail.com>
2025-11-25 09:24:29 -0800

    fixed menus and loops for death and reset

--- hugh-fungus/scripts/death_menu.gd
+++ hugh-fungus/scripts/death_menu.gd
@@ -11,12 +11,20 @@ func _process(delta: float) -> void:
 	#change this from player health to a signal from the player its anims are done 
 	if PlayerHealthGlobal.player_health <= 0:
 		self.visible = true
-	else:
-		self.visible = false
+		get_tree().paused = true
+	
+		
 
 func _on_reset_button_button_up() -> void:
-	get_tree().reload_current_scene()
+	get_tree().paused = false
+	self.visible = false
 	PlayerHealthGlobal.player_health = 5
-
+	get_tree().reload_current_scene()
+	
+	self.visible = false
 func _on_quit_button_button_up() -> void:
+	get_tree().paused = false
+	self.visible = false
+	PlayerHealthGlobal.player_health = 5
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	
