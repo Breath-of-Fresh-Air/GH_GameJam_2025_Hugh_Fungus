1712d95e90f3338e57a65de2c194a5158404d4ca
morgan <goombaplayz@gmail.com>
2025-11-27 15:29:59 -0800

    beehive is smashable
    once picked up honey comb is in hud

--- hugh-fungus/scripts/player_hud.gd
+++ hugh-fungus/scripts/player_hud.gd
@@ -3,10 +3,29 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$honeycomb/TextureRect.visible = false
+	$mycelium/TextureRect.visible = false
+	$mycelium2/TextureRect.visible = false
+	$mycelium3/TextureRect.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-		
+	if MyceliumTracker.items_collected >=3:
+		$mycelium/TextureRect.visible = true
+		$mycelium2/TextureRect.visible = true
+		$mycelium3/TextureRect.visible = true
+	if MyceliumTracker.items_collected ==2 :
+		$mycelium/TextureRect.visible = true
+		$mycelium2/TextureRect.visible = true
+		$mycelium3/TextureRect.visible = false
+	if MyceliumTracker.items_collected == 1 :
+		$mycelium/TextureRect.visible = true
+		$mycelium2/TextureRect.visible = false
+		$mycelium3/TextureRect.visible = false
+	if MyceliumTracker.items_collected <= 0  :
+		$mycelium/TextureRect.visible = false
+		$mycelium2/TextureRect.visible = true
+		$mycelium3/TextureRect.visible = false
+	if MyceliumTracker.honey_comb >= 1:
+		$honeycomb/TextureRect.visible = true
