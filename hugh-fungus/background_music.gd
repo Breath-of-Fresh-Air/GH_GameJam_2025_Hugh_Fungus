c754b5799859c6f5b8a8c4663fde65cb4755610a
Breath-of-Fresh-Air <bofa.opensource@gmail.com>
2025-12-01 06:28:13 -0800

    added tracks, playlists

--- hugh-fungus/background_music.gd
+++ hugh-fungus/background_music.gd
@@ -7,14 +7,42 @@ var MetroCityZone = preload("res://audio/OST/Sonic 2 - Metro City Zone.mp3")
 var theyreComing = preload("res://audio/OST/They're coming!.mp3")
 var VincetheUnwise = preload("res://audio/OST/VincentTheUnwise.mp3")
 var scummBar = preload("res://audio/OST/The Scumm Bar.mp3")
+var rats = preload("res://audio/OST/rats.mp3")
 
-
+#split into distinct areas
+var ground_playlist = [Objection, rats]
+var treetio_playlist = [theyreComing, MetroCityZone, ElderForest]
+var firstGroundPlayed #check if the first song has been played yet before random
 
 #we never want to play the same track twice in a row. 
 var last_track = null
 
 
+var current_area_playlist = null  # keeps track of the current area
+
 func _ready():
-	#play the loaded streamable content
+	# Player spawns in ground level first
+	current_area_playlist = ground_playlist
+	play_area_track(current_area_playlist)
+
+func play_area_track(area_playlist):
+	current_area_playlist = area_playlist  # update current area
+	var next_track
+
+	# Ground area: first play is fixed
+	if area_playlist == ground_playlist and !firstGroundPlayed:
+		next_track = Objection
+		firstGroundPlayed = true
+	else:
+		next_track = area_playlist[randi() % area_playlist.size()]
+		while next_track == last_track and area_playlist.size() > 1:
+			next_track = area_playlist[randi() % area_playlist.size()]
+
+	last_track = next_track
+	stream = next_track
 	play()
-	stop()
+
+func _on_track_finished():
+	# Play another track from the current area
+	if current_area_playlist:
+		play_area_track(current_area_playlist)
